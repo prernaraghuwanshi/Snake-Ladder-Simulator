46e67b7a8586a22b994e8b5df028d687294f9dd1
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-22 16:24:14 +0530

    [Prerna]ADDED: No Play/Ladder/Snake criteria

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,6 +9,24 @@ public class SnakeAndLadder{
 		int die = (int) (Math.random()*6) + 1; 
 		System.out.println("Dice Roll: " + die);
 
+		int option = (int) Math.floor(Math.random() * 10) % 3;
+		if( option == 0)
+		{
+			//No Play
+			positionPlayer1=positionPlayer1;
+		}
+		else if (option == 1)
+		{
+			//Ladder
+			positionPlayer1 += die;
+		}
+		else if (option == 2)
+		{
+			//Snake
+			positionPlayer1 -= die;
+		}
+		System.out.println("Current Position: "+positionPlayer1);
+
 	}
 
 
