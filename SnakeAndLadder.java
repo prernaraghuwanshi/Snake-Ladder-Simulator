3b05e72d793dc5c937ad3c09543f8c5f93b9f68d
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-22 16:04:30 +0530

    [Prerna]ADDED: Basic initializations

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+public class SnakeAndLadder{
+	public static void main(String args[])
+	{
+		int START = 0;
+		int END = 100;
+		int positionPlayer1 = 0;
+		System.out.println("Welcome to Snake and Ladder Simulator!");
+
+	}
+
+
+}
