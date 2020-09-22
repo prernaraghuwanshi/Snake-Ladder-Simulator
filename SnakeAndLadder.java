f15e80d44a9baac842b044daa3873b97fa6b9ebd
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-22 16:34:14 +0530

    [Prerna]ADDED: while loop till Player reaches winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,28 +5,31 @@ public class SnakeAndLadder{
 		int END = 100;
 		int positionPlayer1 = 0;
 		System.out.println("Welcome to Snake and Ladder Simulator!");
-
-		int die = (int) (Math.random()*6) + 1; 
-		System.out.println("Dice Roll: " + die);
-
-		int option = (int) Math.floor(Math.random() * 10) % 3;
-		if( option == 0)
+		while(positionPlayer1 != 100)
 		{
-			//No Play
-			positionPlayer1=positionPlayer1;
-		}
-		else if (option == 1)
-		{
-			//Ladder
-			positionPlayer1 += die;
-		}
-		else if (option == 2)
-		{
-			//Snake
-			positionPlayer1 -= die;
-		}
-		System.out.println("Current Position: "+positionPlayer1);
+			int die = (int) (Math.random()*6) + 1; 
+			System.out.println("Dice Roll: " + die);
 
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+			if( option == 0)
+			{
+				//No Play
+				positionPlayer1=positionPlayer1;
+			}
+			else if (option == 1)
+			{
+				//Ladder
+				positionPlayer1 += die;
+			}
+			else if (option == 2)
+			{
+				//Snake
+				positionPlayer1 -= die;
+			}
+			if(positionPlayer1< 0)
+				positionPlayer1 = 0;
+			System.out.println("Current Position: "+positionPlayer1);
+		}
 	}
 
 
